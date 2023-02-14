11188b6677407f62075830684d0d012958da3c9a
Kyle Schaudt <77643903+frogsupport@users.noreply.github.com>
2023-02-13 21:35:37 -0600

    remove my-app folder

--- src/components/ProtoDemo.js
+++ src/components/ProtoDemo.js
@@ -1,18 +1,16 @@
-import React from 'react'
-import UseFetch from './UseFetch';
+import React from "react";
+import UseFetch from "./UseFetch";
 
 const ProtoDemo = () => {
   // deconstructs the data, loading, and error from the UseFetch function
   // data is the data from the fetch
   // this sets the data given from the fetch to the data variable
   // same with loading and error
-    const { data, loading, error } = UseFetch('https://localhost:3000/api/fundraisers');
-    console.log(data)
-  return (
-    <div>
-        {data}
-    </div>
-  )
-}
+  const { data, loading, error } = UseFetch(
+    "https://localhost:7000/api/fundraisers"
+  );
+  console.log(data);
+  return <div>{data}</div>;
+};
 
 export default ProtoDemo;
