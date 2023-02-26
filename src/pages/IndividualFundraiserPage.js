c9325df0e1a0c44e1df46a82d6130b76757aeb0a
Kyle Schaudt <77643903+frogsupport@users.noreply.github.com>
2023-02-26 16:41:01 -0600

    ramoved unused fcn

--- src/pages/IndividualFundraiserPage.js
+++ src/pages/IndividualFundraiserPage.js
@@ -1,6 +1,5 @@
 import React, {useState, useEffect} from 'react'
 import {  useParams } from "react-router-dom";
-import UseFetch from '../components/UseFetch'
 
 const IndividualFundraiserPage = () => {
     // const [fundraiserRequestOptions, setFundraiserRequestOptions] = useState({});
