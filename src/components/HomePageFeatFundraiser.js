3740cbb285581145c12bf3051c95120f82882153
Austin Nguyen <austinkn123@gmail.com>
2023-02-28 18:55:43 -0600

    Added Link

--- src/components/HomePageFeatFundraiser.js
+++ src/components/HomePageFeatFundraiser.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import placeHolderFundraiser from '../imgs/placeHolderFundraiserPic.jpg';
 
 const HomePageFeatFundraiser = (props) => {
