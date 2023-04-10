082b65251c6a94f0326564a10c04c14261ecf3aa
jthom21 <jtn4c@umsystem.edu>
2023-04-09 19:24:15 -0500

    updated fundraiser display

--- src/components/HomePageComponents/FundraiserDisplaySection.js
+++ src/components/HomePageComponents/FundraiserDisplaySection.js
@@ -1,18 +1,38 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import placeHolderFundraiser from '../../imgs/placeHolderFundraiserPic.jpg';
 
 
 const FundraiserDisplaySection = (props) => {
   return (
-    <div className='w-96 h-fit bg-green-600 truncate text-white'>
-        <img src={placeHolderFundraiser} alt="Logo" />
-        <div className='p-4'>
-            <h1 className='text-2xl mb-4 truncate'>
-              {props.title}
+    <div className='flex w-96 h-fit  text-black'>
+        {<img src={placeHolderFundraiser} alt="Logo" />}
+        <div className='p-4 z-125'>
+
+            <h1 className='text-2xl mb-4 truncate hover:underline hover:text-blue-500 '>
+                <Link to={`/organizer/${props.organizerId}/fundraiser/${props.fundraiserId}`} >
+                    {props.title}
+                </Link>
             </h1>
-            {/* {props.id} */}
+
+         {/* {props.id} */} 
             <p className='truncate '>
                 {props.description}
+            </p>
+            <p>
+                {props.tags ?? 'Tags'}
+            </p>
+            <p>
+                Organizer: {props.organizerName}
+            </p>
+            <p>
+                Contact: {props.organizerEmail}
+            </p>
+            <p>
+                Started Date: {props.createdDate}
+            </p>
+            <p>
+                Planned End Date: {props.createdDate}
             </p>
         </div>
     </div>
