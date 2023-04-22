7875280312b1785f90b377ece938938943d4ba99
Austin Nguyen <austinkn123@gmail.com>
2023-04-22 00:17:16 -0500

    changed font for approved page

--- src/components/ApprovedDonationPageComponents/ApprovedDonationWindow.js
+++ src/components/ApprovedDonationPageComponents/ApprovedDonationWindow.js
@@ -6,14 +6,14 @@ const ApprovedDonationWindow = () => {
     const navigate = useNavigate()
   return (
     <div className='flex flex-col text-center p-8 bg-gray-100 '>
-        <div className='font-semibold text-5xl p-8 font-Approved'>
+        <div className='font-semibold text-5xl p-8'>
             Approved!
         </div>
         <div className='flex justify-center'>
             <FcApproval size={200}/>
         </div>
         
-        <div className='font-semibold text-2xl p-8 font-Approved'>
+        <div className='font-semibold text-2xl p-8 '>
             Thank you for your support!
         </div>
         <div className='p-8'>
